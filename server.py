7600f791d0ca2d1dc25481df5c62fbc6f65102c2
southzyzy <tanzhaoyea98.tzy@gmail.com>
2019-03-23 22:29:58 +0800

    server site validation done

--- server.py
+++ server.py
@@ -1,67 +1,77 @@
-import socket, select
+import socket, select, re
 import threading
 import socketserver
 
 BUFLEN = 8192
 __version__ = '0.1.0 Draft 1'
 VERSION = 'Proxy/' + __version__
+TIMEOUT = 20
+
+
+def url_validation(url):
+    url_valid = re.match("^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$", url)
+    if not url_valid:
+        return False
+
+    return True
 
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
         self.buffer_data = self.request.recv(BUFLEN)
-
-        self.conn_method, self.url, self.protocol, self.host = self.data_handler(str(self.buffer_data, 'ascii'))
+        self.conn_method, self.protocol, self.host = self.data_handler(str(self.buffer_data, 'ascii'))
 
         if self.conn_method == 'CONNECT':
             self.connect(self.host)
-            self.request.send(
-                bytes(self.protocol + ' 200 Connection established\n' + 'Proxy-agent: %s\n\n' % VERSION, 'utf-8'))
+            self.request.send(bytes(self.protocol + ' 200 Connection established\n' + 'Proxy-agent: %s\n\n' % VERSION, 'utf-8'))
             self.buffer_data = b''
             self.send()
 
         elif self.conn_method in ['OPTIONS', 'GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'TRACE']:
-            http_path = self.url[7:]
-            slash = http_path.find('/')
-            host = http_path[:slash]
-
-            self.connect(host)
+            self.connect(self.host)
             self.client_sock.send(self.buffer_data)
 
             self.buffer_data = b''
             self.send()
 
         self.request.close()
-        self.request.server_close()
 
     def data_handler(self, data):
         data_split = data.split()
         conn_method = data_split[0]
-        url = data_split[1]
+
         protocol = data_split[2]
+
         host = data_split[4]
-        return (conn_method, url, protocol, host)
+        if url_validation(host) == False:
+            print("Closing Connection")
+            self.request.close()
+
+        return (conn_method, protocol, host)
 
     def connect(self, host):
-        host_port = host.find(":")
-        if host_port != -1:
-            port_no = int(host[host_port + 1:])
-            host = host[:host_port]
+        host_port = re.search("(.*):(\d+)", host)
+        if host_port:
+            host = host_port.group(1)
+            port_no = int(host_port.group(2))
         else:
             port_no = 80
 
         # Get the address information
         address = (host, port_no)
 
-        # create the socket connection
-        self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_sock.connect(address)
+        try:
+            # create the socket connection
+            self.client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.client_sock.connect(address)
+        except:
+            pass
 
     def send(self):
         socs = [self.request, self.client_sock]
-        count = 0
+        time_counter = 0
         while True:
-            count += 1
+            time_counter += 1
             (recv, _, error) = select.select(socs, [], socs, 3)
             if error:
                 break
@@ -74,7 +84,10 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
                         out = self.request
                     if data:
                         out.send(data)
-                        count = 0
+                        time_counter = 0
+
+            if time_counter == TIMEOUT:
+                break
 
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
